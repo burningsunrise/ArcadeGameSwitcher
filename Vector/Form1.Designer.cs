bb095a546014e8dd96dc97f4686b964fd66e4a7f
burningsunris <nvreaver@gmail.com>
2019-08-12 13:38:22 -0700

    Start the Form Minimized

--- Vector/Form1.Designer.cs
+++ Vector/Form1.Designer.cs
@@ -64,6 +64,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "VectorS";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
